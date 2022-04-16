942a4c1543db29c03436f9e689541f2b8ac11064
kashikichi <60090083+Pikselas@users.noreply.github.com>
2022-04-16 11:56:06 +0530

    MultiWindow Image testing

--- Penther/EntryPoint.cpp
+++ Penther/EntryPoint.cpp
@@ -1,60 +1,101 @@
 #include"Window.h"
 #include"Canvas2D.h"
 
-int WINAPI wWinMain(_In_ HINSTANCE,_In_opt_ HINSTANCE ,_In_ LPWSTR,_In_ int)
+#define Binder1(func , param) std::bind(func , param , std::placeholders::_1)
+#define Binder2(func , param1 , param2)  std::bind(func , param1 , param2 , std::placeholders::_1)
+
+struct POS_T
 {
-	Window wnd("wnd" , 900 , 600);
-	Canvas2D cntx { wnd };
-
-	Image2D img(L"C:/Users/Aritra Maji/Downloads/93428795_p0.png");
-
-	wnd.keyboard.OnKeyPress = [&](Window::KeyBoard::EventT ev) {
-		if (ev.KEY_CODE == 'C')
-		{
-			cntx.Clear();
-		}
-	};
-
 	int x_pos = 0, y_pos = 0;
 	int img_xpos = 0, img_ypos = 0;
-
-	wnd.mouse.OnRightPress = [&](Window&) {
-	
-		x_pos = wnd.mouse.GetX();
-		y_pos = wnd.mouse.GetY();
-		
-	};
-
-	wnd.mouse.OnRightRelease = [&](Window&) {
-		
-		img_xpos +=  wnd.mouse.GetX() - x_pos;
-		img_ypos +=  wnd.mouse.GetY() - y_pos;
-	};
-
-	wnd.mouse.OnMove = [&](Window&) mutable {
-		if (wnd.mouse.IsLeftPressed())
-		{
-			auto[x, y] = wnd.mouse.GetXY();
-			const auto half = wnd.GetWidth() / 2;
-			auto x1 = x < half ? half + (half - x) : half - (x - half);
-			cntx.DrawPixel(x, y, { 255 , 255 , 255 });
-			cntx.DrawPixel(x1, y, { 255 , 255 , 255 });
-		}
-		else if(wnd.mouse.IsRightPressed())
-		{
-			cntx.Clear();
-			cntx.DrawImage(img, img_xpos + wnd.mouse.GetX() - x_pos, img_ypos + wnd.mouse.GetY() - y_pos);
-		}
-		
-	};
-
-
-	cntx.DrawImage(img);
-
-	while (wnd.IsOpen())
+};
+void keyboardPress(Canvas2D& cntx , Window::KeyBoard::EventT ev)
+{
+	if (ev.KEY_CODE == 'C')
 	{
-		wnd.ProcessEvents();
+		cntx.Clear();
+	}
+}
+void MouseRightPress(Canvas2D& cntx, Window& wnd)
+{
+	POS_T* const pos_t = static_cast<POS_T*>(wnd.UserSideDataPointer);
+	pos_t->x_pos = wnd.mouse.GetX();
+	pos_t->y_pos = wnd.mouse.GetY();
+}
+void MouseRightRelease(Canvas2D& cntx, Window& wnd)
+{
+	POS_T* const pos_t = static_cast<POS_T*>(wnd.UserSideDataPointer);
+	pos_t->img_xpos += wnd.mouse.GetX() - pos_t->x_pos;
+	pos_t->img_ypos += wnd.mouse.GetY() - pos_t->y_pos;
+}
+void MouseMove(Canvas2D& cntx, Image2D& img , Window& wnd)
+{
+	POS_T* const pos_t = static_cast<POS_T*>(wnd.UserSideDataPointer);
+	if (wnd.mouse.IsLeftPressed())
+	{
+		auto [x, y] = wnd.mouse.GetXY();
+		const auto half = wnd.GetWidth() / 2;
+		auto x1 = x < half ? half + (half - x) : half - (x - half);
+		cntx.DrawPixel(x, y, { 255 , 255 , 255 });
+		cntx.DrawPixel(x1, y, { 255 , 255 , 255 });
 		cntx.DrawOnWindow();
+	}
+	else if (wnd.mouse.IsRightPressed())
+	{
+		cntx.Clear();
+		cntx.DrawImage(img, pos_t->img_xpos + wnd.mouse.GetX() - pos_t->x_pos, pos_t->img_ypos + wnd.mouse.GetY() - pos_t->y_pos);
+		cntx.DrawOnWindow();
+	}
+}
+
+int WINAPI wWinMain(_In_ HINSTANCE,_In_opt_ HINSTANCE ,_In_ LPWSTR,_In_ int)
+{
+	Window wnd1("wnd1" , 500 , 700);
+	Window wnd2("wnd2", 500, 700);
+	Window wnd3("wnd3", 500, 700);
+	
+	Canvas2D cntx1 { wnd1 };
+	Canvas2D cntx2 { wnd2 };
+	Canvas2D cntx3 { wnd3 };
+
+	Image2D img1(L"C:/Users/Aritra Maji/Downloads/93428795_p0.png");
+	Image2D img2(L"C:/Users/Aritra Maji/Downloads/mona-finden-lovestory.jpg");
+	Image2D img3(L"C:/Users/Aritra Maji/Downloads/japan/31020.jpg");
+
+	POS_T pst1;
+	POS_T pst2;
+	POS_T pst3;
+
+	wnd1.UserSideDataPointer = &pst1;
+	wnd2.UserSideDataPointer = &pst2;
+	wnd3.UserSideDataPointer = &pst3;
+
+	wnd1.keyboard.OnKeyPress  =	Binder1(keyboardPress , std::ref(cntx1));
+	wnd1.mouse.OnRightPress   =	Binder1(MouseRightPress , std::ref(cntx1));
+	wnd1.mouse.OnRightRelease =	Binder1(MouseRightRelease, std::ref(cntx1));
+	wnd1.mouse.OnMove         =	Binder2(MouseMove, std::ref(cntx1), std::ref(img1));
+
+	wnd2.keyboard.OnKeyPress  = Binder1(keyboardPress, std::ref(cntx2));
+	wnd2.mouse.OnRightPress   = Binder1(MouseRightPress, std::ref(cntx2));
+	wnd2.mouse.OnRightRelease = Binder1(MouseRightRelease, std::ref(cntx2));
+	wnd2.mouse.OnMove		  = Binder2(MouseMove, std::ref(cntx2), std::ref(img2));
+
+	wnd3.keyboard.OnKeyPress  = Binder1(keyboardPress, std::ref(cntx3));
+	wnd3.mouse.OnRightPress   = Binder1(MouseRightPress, std::ref(cntx3));
+	wnd3.mouse.OnRightRelease = Binder1(MouseRightRelease, std::ref(cntx3));
+	wnd3.mouse.OnMove	      = Binder2(MouseMove, std::ref(cntx3), std::ref(img3));
+
+	cntx1.DrawImage(img1);
+	cntx2.DrawImage(img2);
+	cntx3.DrawImage(img3);
+	
+	cntx1.DrawOnWindow();
+	cntx2.DrawOnWindow();
+	cntx3.DrawOnWindow();
+
+	while (Window::GetWindowCount())
+	{
+		Window::ProcessWindowEvents();
 	}
 	return 0;
 }
